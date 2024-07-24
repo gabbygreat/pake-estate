21215d5c796dea2cb720f04cd55f0afd32d06696
Son-O-b-k <sonobktech@gmail.com>
2024-07-24 18:55:40 +0100

    updated

--- start/routes/property-route.ts
+++ start/routes/property-route.ts
@@ -2,9 +2,10 @@ import router from "@adonisjs/core/services/router";
 import PropertyController from "#controllers/properties_controller";
 
 router.group(()=>{
-    router.post('composeProperty',[PropertyController,'composeProperty'])
-    router.delete('removePropertyMedia',[PropertyController,'removePropertyMedia'])
-    router.get('listPropertyMedia',[PropertyController,'listPropertyMedia'])
-    router.get('listProperties',[PropertyController,'listProperties'])
+
+    router.post('compose-property',[PropertyController,'composeProperty'])
+    router.delete('remove-property-media',[PropertyController,'removePropertyMedia'])
+    router.get('list-property-media',[PropertyController,'listPropertyMedia'])
+    router.get('list-properties',[PropertyController,'listProperties'])
 
 }).prefix('/property')
