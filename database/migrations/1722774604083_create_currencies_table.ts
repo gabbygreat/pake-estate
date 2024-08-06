624dd2ef2b5db5f5d65544f49dcc97b100bb0420
Zumbaski <henryilozumba@gmail.com>
2024-08-06 21:04:08 +0100

    :fire:"added currency and client_currency migrations"

--- database/migrations/1722774604083_create_currencies_table.ts
+++ database/migrations/1722774604083_create_currencies_table.ts
@@ -5,7 +5,16 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      //table.increments('id')
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+      table.string('name').nullable()
+      table.string('symbol').nullable()
+      table.string('symbol_native').nullable()
+      table.string('decimal_digits').nullable()
+      table.string('rounding').nullable()
+      table.string('code').nullable()
+      table.string('name_plural').nullable()
+      table.boolean('supported').nullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
