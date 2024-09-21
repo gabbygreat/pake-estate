4809d0060a4121d1a96bbf05ad17a97c59ef05d5
Son-O-b-k <sonobktech@gmail.com>
2024-09-21 21:34:17 +0100

    wallet controller setup

--- app/controllers/wallets_controller.ts
+++ app/controllers/wallets_controller.ts
@@ -1,4 +1,20 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+//import type { HttpContext } from '@adonisjs/core/http'
 
 export default class WalletsController {
+
+    async fundWallet(){
+
+    }
+
+    async walletBalanceInfo(){
+
+    }
+
+    async withdraw(){
+
+    }
+
+    async transfer(){
+
+    }
 }
