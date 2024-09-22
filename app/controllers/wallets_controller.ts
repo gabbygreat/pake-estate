816193a04ec0e025d7f5d0928d9797e6b34290e1
Son-O-b-k <sonobktech@gmail.com>
2024-09-22 08:50:08 +0100

    updated for deleting property

--- app/controllers/wallets_controller.ts
+++ app/controllers/wallets_controller.ts
@@ -3,18 +3,42 @@
 export default class WalletsController {
 
     async fundWallet(){
-
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 
     async walletBalanceInfo(){
-
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 
     async withdraw(){
-
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 
     async transfer(){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
 
+    async transactionHistory(){
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 }
--- start/routes/property-route.ts
+++ start/routes/property-route.ts
@@ -15,6 +15,6 @@ router.group(()=>{
     router.get('top-selling',[PropertyController,'topSellingProperties'])
     router.patch('publish',[PropertyController,'publishProperty']).use(middleware.auth({guards:['api']}))
     router.get('hide-property/:id',[PropertyController,'hideProperty']).use(middleware.auth({guards:['api']}))
-    //router.delete('remove/:id',[PropertyController,'deleteProperty'])
+    router.delete('remove/:id',[PropertyController,'deleteProperty']).use(middleware.auth({guards:['api']}))
     
 }).prefix('/property')
