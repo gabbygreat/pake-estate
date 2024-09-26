4aec32853c80124ea97820b50622ffe28035da04
Son-O-b-k <sonobktech@gmail.com>
2024-09-26 19:11:09 +0100

    updated

--- app/controllers/maintenance_controller.ts
+++ app/controllers/maintenance_controller.ts
@@ -82,7 +82,7 @@ export default class MaintenanceController {
             const req = await MaintenanceRequest.find(id)
             if(req && initiator){
                 console.log("owner ",req.owner_id,' AND applicant id ',req.applicant_id," current user ", initiator.id)
-                if(initiator.id !== req.owner_id || initiator.id !== req.applicant_id){
+                if(initiator.id != req.owner_id || initiator.id != req.applicant_id){
                     return sendError(response,{message:"You are not authorized to perform this operation", code:403})
                 }
                 for(const image of req.images){
