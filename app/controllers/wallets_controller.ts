0274c0ae1e2b2afa004c8a0d3c674104ea55dd90
Son-O-b-k <sonobktech@gmail.com>
2024-10-23 07:09:58 +0100

    updated transaction history

--- app/controllers/wallets_controller.ts
+++ app/controllers/wallets_controller.ts
@@ -134,13 +134,13 @@ export default class WalletsController {
             .select('*')
             .where('wallet_id','=',input.wallet_id!)
 
-            if(input.status && input.status !== undefined && input.status !== 'undefined'){
+            if(input.status && input.status !== undefined && input.status !== 'undefined' && input.status !== 'null' && input.status !== null){
                 dataQuery.andWhere('payment_status','=',input.status)
             }
-            if(input.search && input.search !== undefined && input.search !== 'undefined'){
+            if(input.search && input.search !== undefined && input.search !== 'undefined' && input.search !== 'null' && input.search !== null){
                 dataQuery.andWhere((q)=>q.whereRaw(`description % ?`,[input.search!]))
             }
-            if(input.start && input.start !== undefined && input.end && input.end !== undefined){
+            if(input.start && input.start !== undefined && input.end && input.end !== undefined && input.end !== 'null' && input.end !== null && input.start !== 'null' && input.start !== null){
                 dataQuery.andWhereBetween('updated_at',[input.start,input.end])
             }
             if(input.type){
